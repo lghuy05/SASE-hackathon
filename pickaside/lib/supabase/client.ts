c96f86585bf66fa6122ce4b7922c8f37f39a5585
Davii-Ng <nminhd98@gmail.com>
2025-09-21 07:17:26 +0700

    update sign in sign up

--- pickaside/lib/supabase/client.ts
+++ pickaside/lib/supabase/client.ts
@@ -1,6 +1,10 @@
 import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
+  console.log("URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+  console.log("Key:", process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_OR_ANON_KEY);
+
+
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_OR_ANON_KEY!,
